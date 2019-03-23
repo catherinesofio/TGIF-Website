b03f9ed38647ff0b83a661f0b781673ce76a245f
catherinesofio <catalinasofioavogadro@hotmail.com>
2019-03-23 13:56:41 -0300

    Fixes
    * Indemnation.
    * Solved last exercise.

--- root/scripts/basics.js
+++ root/scripts/basics.js
@@ -2,31 +2,35 @@
 let myName = "Catalina";
 console.log(myName);
 
+
 //Exercise 2
 let age = 21;
 console.log(age);
+
 
 //Exercise 3
 let ignasiAge = 32;
 let ageDiff = Math.abs(age - ignasiAge);
 console.log(ageDiff);
 
+
 //Exercise 4
 let num = 21;
 
 if (age > num) {
-	console.log(myName + " is older than a " + num + " yeard old.");
+  console.log(myName + " is older than a " + num + " yeard old.");
 } else if (age < num) {
-	console.log(myName + " is younger than a " + num + " yeard old.");
+  console.log(myName + " is younger than a " + num + " yeard old.");
 } else {
-	console.log(myName + " is " + num + " years old.");
+  console.log(myName + " is " + num + " years old.");
 }
+
 
 //Exercise 5
 if (age > ignasiAge) {
-	console.log(myName + "is older than Ignasi.");
+  console.log(myName + "is older than Ignasi.");
 } else if (age < ignasiAge) {
-	console.log("Ignasi is older than " + myName + ".");
+  console.log("Ignasi is older than " + myName + ".");
 } else {
-	console.log("Both Ignasi and " + myName + " are of the same age.");
+  console.log("Both Ignasi and " + myName + " are of the same age.");
 }
--- root/scripts/functions.js
+++ root/scripts/functions.js
@@ -4,15 +4,16 @@ let ex1 = -32443;
 console.log(ReverseNumber(ex1));
 
 function ReverseNumber(x) {
-	let temp = x.toString();
-	let xReversed = "";
+  let temp = x.toString();
+  let xReversed = "";
 
-	for (let i = temp.length - 1; i >= 0; i--) {
-		xReversed += temp[i];
-	}
+  for (let i = temp.length - 1; i >= 0; i--) {
+    xReversed += temp[i];
+  }
 
-	return parseInt(xReversed) * Math.sign(x);
+  return parseInt(xReversed) * Math.sign(x);
 }
+
 
 //Exercise 2
 let ex2 = "webmaster";
@@ -20,8 +21,9 @@ let ex2 = "webmaster";
 console.log(OrderString(ex2));
 
 function OrderString(x) {
-	return x.split('').sort().join('')
+  return x.split('').sort().join('')
 }
+
 
 //Exercise 3
 let ex3 = "web development tutorial"
@@ -29,31 +31,33 @@ let ex3 = "web development tutorial"
 console.log(CapitaliseString(ex3));
 
 function CapitaliseString(x) {
-	let temp = x.split(" ");
+  let temp = x.split(" ");
 
-	for (let i = 0; i < temp.length; i++) {
-		temp[i] = temp[i].charAt(0).toUpperCase() + temp[i].slice(1);
-	}
+  for (let i = 0; i < temp.length; i++) {
+    temp[i] = temp[i].charAt(0).toUpperCase() + temp[i].slice(1);
+  }
 
-	return temp.join(" ");
+  return temp.join(" ");
 }
+
 
 //Exercise 4
 console.log(GetLargestWord(ex3));
 
 function GetLargestWord(x) {
-	let parts = x.split[" "];
-	let temp = [];
-	
-	for (let i = 0; i < parts.length; i++) {
-		temp.push(parts[i].split[''].length);
-	}
-	
-	let index = 0;
-	for (let i = temp.length - 1; i >= 0; i--) {
-		if (temp[index] < temp[i]) {
-			result = i;}
-	}
-	
-	return parts[index];
+  let parts = x.split(" ");
+  let temp = [];
+
+  for (let i = 0; i < parts.length; i++) {
+    temp.push(parts[i].split('').length);
+  }
+
+  let index = 0;
+  for (let i = temp.length - 1; i >= 0; i--) {
+    if (temp[index] < temp[i]) {
+      result = i;
+    }
+  }
+
+  return parts[index];
 }
