6ccc63023afef396d14c5ed45085999f4ddbedcb
catherinesofio <catalinasofioavogadro@hotmail.com>
2019-03-25 15:02:42 -0300

    Update
    * Changes that were not published.

--- root/scripts/load-data.js
+++ root/scripts/load-data.js
@@ -1,6 +1,6 @@
 "use strict";
 
-let e = document.createElement('div');
+let e = document.createElement(document.currentScript.getAttribute('elementTag'));
 e.innerHTML =  JSON.stringify(data);
 e.setAttribute('class', 'data');
 document.body.appendChild(e);
